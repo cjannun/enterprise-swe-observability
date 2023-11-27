55798e86002983f833d558de34fa605a51cefdc4
cjannun <93235878+cjannun@users.noreply.github.com>
2023-11-26 21:15:58 -0500

    Finished code

--- tracing.js
+++ tracing.js
@@ -3,10 +3,13 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
 const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+
 // Instrumentations
+
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
@@ -14,7 +17,10 @@ const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 
 // Exporter
 module.exports = (serviceName) => {
-  const exporter = new ConsoleSpanExporter();
+  const exporter = new JaegerExporter({
+    serviceName: serviceName,
+  });
+
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
